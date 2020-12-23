2340a5827ed154e82d652d8439f3dfe8e2e8e361
Vera Chikhareva <vera.chepovaya@mail.ru>
2020-12-23 11:24:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price=15725;
+        int mile=20;
+        int bonus=price*1/mile;
+        System.out.println(bonus);
+
+    }
+
+}
