a60964bedc0e32cab60b684451ddcc848342daee
Vera Chikhareva <vera.chepovaya@mail.ru>
2020-12-24 13:36:56 +0300

    add changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price=15725;
-        int mile=20;
-        int bonus=price*1/mile;
+        int price = 1_587_528;
+        int mile = 20;
+        int bonus = price / mile;
         System.out.println(bonus);
 
     }
